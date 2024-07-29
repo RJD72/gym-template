79e47d0a7bbe62538b5446aabf1a43ab437458c5
robdeer1 <r_deer@fanshaweonline.ca>
2024-07-29 00:51:21 -0400

    made sign in link dynamic

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { Auth, authState } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -8,4 +11,27 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  link = {
+    text: 'Sign In',
+    route: '/admin',
+  };
+
+  user: Observable<any>;
+
+  constructor(private auth: Auth) {
+    this.user = authState(this.auth);
+  }
+
+  ngOnInit() {
+    this.user.subscribe((user) => {
+      if (user) {
+        this.link.text = 'Input';
+        this.link.route = '/input';
+      } else {
+        this.link.text = 'Sign In';
+        this.link.route = '/admin';
+      }
+    });
+  }
+}
